ca1f4cfbcaec50170d5a4f2dbc0b31cb78e43a38
Danial Iranmehr <danidaniran6@gmail.com>
2024-09-15 18:33:18 +0330

    - some gradle file update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,14 @@ android {
             "\"${project.findProperty("PORTAL_API_KEY")}\""
         )
     }
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = false
+        }
+    }
 
     buildTypes {
         release {
